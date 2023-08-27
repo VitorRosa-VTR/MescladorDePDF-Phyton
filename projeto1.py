a0ce08d6d814803e88a516d58d48fa0cbe23cb2d
VitorRosa-VTR <Vitorrosa0610@gmail.com>
2023-08-27 11:56:36 -0300

    Resolvendo erro pt01

--- projeto1.py
+++ projeto1.py
@@ -3,12 +3,12 @@ import os
 
 merger = PyPDF2.PdfMerger()
 
-lista_arquivos = os.listdir("arquivos_para_mesclar")
+lista_arquivos = os.listdir("arquivos")
 lista_arquivos.sort()
 print(lista_arquivos)
 
 for arquivo in lista_arquivos:
-    if ".pdf" in arquivos_para_mesclar:
-        merger.append(f"arquivos_para_mesclar/{arquivo}")
+    if ".pdf" in arquivo:
+        merger.append(f"arquivos/{arquivo}")
 
-merger.write("PDF Final.pdf")
+merger.write("PDF Mesclado.pdf")
