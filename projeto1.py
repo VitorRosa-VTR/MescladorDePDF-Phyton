c25ace34f7fcdb11365531cead86ef7582e296d7
VitorRosa-VTR <Vitorrosa0610@gmail.com>
2023-08-27 11:48:28 -0300

    Renomeando Diretório 02

--- projeto1.py
+++ projeto1.py
@@ -3,12 +3,12 @@ import os
 
 merger = PyPDF2.PdfMerger()
 
-lista_arquivos = os.listdir("ArquivosParaMesclar")
+lista_arquivos = os.listdir("arquivos_para_mesclar")
 lista_arquivos.sort()
 print(lista_arquivos)
 
 for arquivo in lista_arquivos:
-    if ".pdf" in arquivo:
-        merger.append(f"arquivos/{arquivo}")
+    if ".pdf" in arquivos_para_mesclar:
+        merger.append(f"arquivos_para_mesclar/{arquivo}")
 
 merger.write("PDF Final.pdf")
